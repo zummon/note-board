09cc73f17aee18a86735c2a6969a19c634263ad0
zummon <zummon.space@gmail.com>
2023-11-12 16:20:20 +0700

    try fix error

--- svelte.config.js
+++ svelte.config.js
@@ -1,12 +1,19 @@
 import adapter from '@sveltejs/adapter-static';
-import { vitePreprocess } from '@sveltejs/kit/vite';
+// import { vitePreprocess } from '@sveltejs/kit/vite';
+import { mdsvex } from 'mdsvex';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
 		adapter: adapter()
 	},
-  preprocess: vitePreprocess()
+	extensions: ['.svelte', '.md', '.svx'],
+  preprocess: [
+		mdsvex({
+			extensions: ['.md', '.svx'],
+		}),
+		// vitePreprocess()
+	]
 };
 
 export default config;
