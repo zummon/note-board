6af7ba2b2155dba294d872efd22f08d12dcdffc6
zummon <zummon.space@gmail.com>
2024-05-18 22:46:08 +0700

    pend

--- src/routes/+page.js
+++
@@ -1,7 +0,0 @@
-export const prerender = true
-
-export async function load({ parent }){
-	let { lang, dark, languages } = await parent()
-
-	return { lang, dark, languages }
-}
--- src/routes/+page.svelte
+++
@@ -1,8 +0,0 @@
-<script>
-  import Home from "../lib/Home.svelte"
-
-  export let data
-  
-</script>
-
-<Home lang={data.lang} dark={data.dark} languages={data.languages} />
